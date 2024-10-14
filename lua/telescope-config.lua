0d2691a28a00ea9dcdca8c2311dd86b24fee085a
Matthew Urry <matthew.urry@amd.com>
2024-10-14 07:47:57 +0100

    add trouble to telescope

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -5,5 +5,16 @@ end
 
 telescope.load_extension('fzf')
 
+local status_ok, trouble = pcall(require, "trouble")
+if not status_ok, then
+  return
+end
 
-
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = { ["<c-t>"] = trouble.sources.telescope.open_with_trouble,
+            n = { ["<c-t>"] = trouble.sources.telescope.open_with_trouble,
+        }
+    }
+})
